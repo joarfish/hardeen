d5f151eebf71780303f2eb359a835cde50883565
kastenbutt <jonas@kleinbildfilm.net>
2019-10-06 20:55:40 +0200

    Change Readme. Add basic library docs for hardeen_core

--- hardeen_core/src/lib.rs
+++ hardeen_core/src/lib.rs
@@ -1,3 +1,17 @@
+//! # Hardeen
+//!
+//! Hardeen is a library which allows for node-based procedural modelling and animation (not yet implemented)
+//! of 2d vector graphics.
+//! 
+//! There are three main components:
+//! - GeometryWorld: A datastructure representing points, shapes and groupings of points
+//! - Graph: An acyclic, directed graph; each Node with an associated processor
+//! - Processors: Processors one or more GeometryWorlds and produce a new one
+//! 
+//! `handled_vec` provides a datastructure that is used throughout the library. Insted of using smart
+//! pointers, Hardeen havily relies on `handles`.
+
+
 mod geometry;
 mod graph;
 mod handled_vec;
